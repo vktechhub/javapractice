5398baec5d75f756bae9967c93c469bd07900684
vktechhub <33621237+vktechhub@users.noreply.github.com>
2025-09-24 04:23:15 +0530

    escape sequence

---
+++ src/CJ_Escape_sequence.java
@@ -0,0 +1,13 @@
+public class CJ_Escape_sequence {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("\t - Insert Tab");
+    System.out.println("\b - Insert Backspace");
+    System.out.println("\n - Insert New line");
+    System.out.println("\r - carriage return. ()");
+    System.out.println("\' - Insert Single quote");
+    System.out.println("\" - Insert Double quote");
+    System.out.println("\\ - Insert Backslash");
+    System.out.println("\f - form feed");
+  }
+}
