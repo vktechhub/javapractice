39f8b904449c7915587333842649ead6a64f1adf
Vigneshkanna <vigneshkanna1985@gmail.com>
2025-10-11 04:25:14 +0530

    Rest controler demo

---
+++ SpringBoot/Restcontrollerdemo/src/main/java/com/vkworks/Restcontrollerdemo/RestcontrollerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.vkworks.Restcontrollerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrollerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcontrollerdemoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Restcontrollerdemo/src/test/java/com/vkworks/Restcontrollerdemo/RestcontrollerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkworks.Restcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrollerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
